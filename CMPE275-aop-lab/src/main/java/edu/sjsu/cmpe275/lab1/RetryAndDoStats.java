dd4450d267db3879f3a2aec4b77a89d8f34510d8
Aniket Khaire <aniketd.khaire@gmail.com>
2016-05-06 10:04:25 -0700

    Implementation files

--- CMPE275-aop-lab/src/main/java/edu/sjsu/cmpe275/lab1/RetryAndDoStats.java
+++ CMPE275-aop-lab/src/main/java/edu/sjsu/cmpe275/lab1/RetryAndDoStats.java
@@ -1,15 +1,149 @@
+//package main.java.edu.sjsu.cmpe275.lab1;
 package edu.sjsu.cmpe275.lab1;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.aop.ProxyMethodInvocation;
+
+import java.io.IOException;
+import java.util.*;
 
 public class RetryAndDoStats implements MethodInterceptor {
     /***
      * Following is the dummy implementation of advice.
      * Students are expected to complete the required implementation as part of lab completion.
      */
+	
+	public static Map<String, List<String>> followers = new HashMap<String, List<String>>();
+	public static Map<String, List<String>> successfulMessages = new HashMap<String, List<String>>();
+	public static Map<String, List<String>> unsuccessfulMessages = new HashMap<String, List<String>>();	
+	
     public Object invoke(MethodInvocation invocation) throws Throwable {
-        System.out.println("Method " + invocation.getMethod() + " is called");
-        return invocation.proceed();
+    	
+    	String methodName = invocation.getMethod().getName();
+    	Object[] arguments = invocation.getArguments();
+    	Object obj= null;
+    	int count = 0;
+    	
+    	try{
+    		//executing the statement
+    		obj = invocation.proceed();
+    		
+    		//if exception is not thrown, and methodName is "tweet", store the tweet as a successful tweet
+    		if(methodName.equals("tweet")){
+    			//for "tweet method"
+    			String userName = arguments[0].toString();
+    			String userTweet = arguments[1].toString();
+    			//storing successful tweets
+    			storeSuccessfulTweets(userName, userTweet);    			
+    		}
+    		else{
+    			//for "follow method"
+    			String follower = arguments[0].toString();
+    			String followee = arguments[1].toString();
+    			//adding follower to followee
+    			addFollower(follower, followee);
+    		}
+    	}//end of try
+    	catch(IOException e){
+    		//retry for 3 times
+    		while(count++ < 3)
+    		{
+    			try{
+    				invocation.proceed();
+    				if(methodName.equals("tweet")){
+    					//fetching arguments
+    					String userName = arguments[0].toString();
+    	    			String userTweet = arguments[1].toString();
+    	    			//storing successful tweets
+    					storeSuccessfulTweets(userName, userTweet);
+    				}
+    				else{
+    					//fetching arguments
+    					String follower = arguments[0].toString();
+    	    			String followee = arguments[1].toString();
+    	    			//adding follower to followee
+    					addFollower(follower, followee);
+    				}
+    				count = 0;
+    				break;
+    			}catch(Exception x){
+    				if(x instanceof IllegalArgumentException){
+    					storeUnsuccessfulTweets(arguments[0].toString(), arguments[1].toString());
+    					count = 0;
+    					break;
+    				}
+    				
+    				System.out.println("Retrying..."+count);
+    			}//end of inner catch
+    		}
+    		
+    		if(count != 0){
+    			//unsuccesful message because of IOException
+    			if(methodName.equals("tweet"))
+        			storeUnsuccessfulTweets(arguments[0].toString(), arguments[1].toString());
+    		}
+
+    		//return obj;
+    		return invocation.proceed();
+    	}//end of outer catch
+    	catch(IllegalArgumentException il){
+    		//Message length >140, thus adding to unsuccessful tweet
+    		storeUnsuccessfulTweets(arguments[0].toString(), arguments[1].toString());
+    	}//end of catch
+    	
+        //System.out.println("Method " + invocation.getMethod() + " is called");
+        //return obj;
+    	return invocation.proceed();
     }
+    
+    
+    //function to write unsuccessful tweets
+    public void storeUnsuccessfulTweets(String userName, String userTweet){
+    	/* storing unsuccessful tweets
+		 */
+    	if(unsuccessfulMessages.containsKey(userName))
+		{
+			List<String> temp = unsuccessfulMessages.get(userName);
+			temp.add(userTweet);
+			unsuccessfulMessages.put(userName, temp);
+		}else
+		{
+			List<String> temp = new ArrayList<String>();
+			temp.add(userTweet);
+			unsuccessfulMessages.put(userName, temp);
+		}
+    }
+    
+  //function to write successful tweets
+    public void storeSuccessfulTweets(String userName, String userTweet){
+    	/* if user already exists, add the tweet to corresponding List<String>
+		 * else add the new user to the Map
+		 */
+		if(successfulMessages.containsKey(userName)){
+			List<String> temp = successfulMessages.get(userName);
+			temp.add(userTweet);
+			successfulMessages.put(userName, temp);
+		}else{
+			List<String> temp = new ArrayList<String>();
+			temp.add(userTweet);
+			successfulMessages.put(userName, temp);
+		}
+    }
+    
+  //function to add follower to followee
+    public void addFollower(String follower, String followee){
+    	/* if user already exists, add the follower to corresponding followee
+		 * else add the new followee to the Map along with its follower 
+		 */
+		if(followers.containsKey(followee)){
+			List<String> temp = followers.get(followee);
+			temp.add(follower);
+			followers.put(followee, temp);
+		}else{
+			List<String> temp = new ArrayList<String>();
+			temp.add(follower);
+			followers.put(followee, temp);
+		}    	
+    }    
 }
--- CMPE275-aop-lab/src/main/java/edu/sjsu/cmpe275/lab1/TweetStatsImpl.java
+++ CMPE275-aop-lab/src/main/java/edu/sjsu/cmpe275/lab1/TweetStatsImpl.java
@@ -1,4 +1,11 @@
+//package main.java.edu.sjsu.cmpe275.lab1;
 package edu.sjsu.cmpe275.lab1;
+
+//import java.util.HashMap;
+import java.util.List;
+//import java.util.Map;
+import java.util.Set;
+import java.util.*;
 
 public class TweetStatsImpl implements TweetStats {
 
@@ -9,30 +16,104 @@ public class TweetStatsImpl implements TweetStats {
 
     @Override
     public void resetStats() {
-        // TODO Auto-generated method stub
-
+        // TODO Auto-generated method stub    	
+    	//Clearing all the data
+    	RetryAndDoStats.followers.clear();
+    	RetryAndDoStats.successfulMessages.clear();
+    	RetryAndDoStats.unsuccessfulMessages.clear();
     }
-
+    
     @Override
     public int getLengthOfLongestTweetAttempted() {
         // TODO Auto-generated method stub
-        return 0;
+    	int length = 0;
+    	
+    	//finding max length of successful messages
+    	for(List<String> data : RetryAndDoStats.successfulMessages.values()){
+    		for(String message : data){
+    			if(message.length() > length)
+    				length = message.length();
+    		}
+    	}
+    	
+    	//finding max length of unsuccessful messages
+    	for(List<String> data : RetryAndDoStats.unsuccessfulMessages.values()){
+    		for(String message : data){
+    			if(message.length() > length)
+    				length = message.length();
+    		}
+    	}
+    	
+        return length;
     }
 
     @Override
     public String getMostFollowedUser() {
         // TODO Auto-generated method stub
-        return null;
+    	int noOfFollowers = 0, followers =0;
+    	List<String> userName = new ArrayList<String>();
+    	
+    	//finding user with maximum number of followers
+    	for(String name : RetryAndDoStats.followers.keySet()){ 
+    		Set s = new HashSet(RetryAndDoStats.followers.get(name));
+    		
+    		followers = s.size();
+    		
+    		//if user with highest followers found, clear the list and add this user
+    		if(followers > noOfFollowers){
+    			noOfFollowers = followers;
+    			userName.clear();
+    			
+    			//add username to list
+    			userName.add(name);    			
+    		}
+    		//if multiple users exist with highest number of followers
+    		else if(followers == noOfFollowers){
+    			userName.add(name);
+    		}
+    		//setting followers to 0 for next user in loop
+    		followers = 0;
+    	}
+    	
+    	//if empty, return NULL
+    	if(userName.isEmpty())
+    		return null;
+    	//else, sort and return smallest element
+    	Collections.sort(userName);
+    	return userName.get(0); 
     }
-
+    
     @Override
     public String getMostProductiveUser() {
         // TODO Auto-generated method stub
-        return null;
+    	int length = 0;
+    	int maxLength = 0;
+    	List<String> userName = new ArrayList<String>();
+    	
+    	//finding max length of successful messages
+    	for(String name : RetryAndDoStats.successfulMessages.keySet()){ 
+    		//calculate total length for particular user
+    		for(String message : RetryAndDoStats.successfulMessages.get(name)){
+    			length += message.length();
+    		}
+    		if(length > maxLength){
+    			maxLength = length;
+    			userName.clear();
+    			
+    			//add username to list
+    			userName.add(name);    			
+    		}
+    		else if(length == maxLength){
+    			userName.add(name);
+    		}
+    		length = 0;
+    	}
+    	
+    	//if empty, return NULL
+    	if(userName.isEmpty())
+    		return null;
+    	//else, sort and return smallest element
+    	Collections.sort(userName);
+    	return userName.get(0);
     }
-    //...
-
 }
-
-
-
